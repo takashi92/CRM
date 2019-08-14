252b9afd2d3fcfc104dceecf6877968295fed316
anonymous031096 <anonymous031096@gmail.com>
2019-08-14 22:48:23 +0700

    update entity

---
+++ backend/salesmanagement/src/main/java/com/lpdev/salesmanagement/entities/Brand.java
@@ -0,0 +1,103 @@
+package com.lpdev.salesmanagement.entities;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "brand", catalog = "sales_management")
+public class Brand implements java.io.Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private String note;
+	private Date created;
+	private Date updated;
+	private Set<Products> productses = new HashSet<Products>(0);
+
+	public Brand() {
+	}
+
+	public Brand(Date created) {
+		this.created = created;
+	}
+
+	public Brand(String name, String note, Date created, Date updated, Set<Products> productses) {
+		this.name = name;
+		this.note = note;
+		this.created = created;
+		this.updated = updated;
+		this.productses = productses;
+	}
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+
+	@Column(name = "id", unique = true, nullable = false)
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Column(name = "name")
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "note")
+	public String getNote() {
+		return this.note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "created", nullable = false, length = 19)
+	public Date getCreated() {
+		return this.created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "updated", length = 19)
+	public Date getUpdated() {
+		return this.updated;
+	}
+
+	public void setUpdated(Date updated) {
+		this.updated = updated;
+	}
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "brand")
+	public Set<Products> getProductses() {
+		return this.productses;
+	}
+
+	public void setProductses(Set<Products> productses) {
+		this.productses = productses;
+	}
+
+}
