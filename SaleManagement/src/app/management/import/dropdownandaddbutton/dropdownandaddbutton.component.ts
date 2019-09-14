28a4d0914042c0e0a29edb5d3c6bb7669d976d73
tunp88 <nguyenphantu88@gmail.com>
2019-09-14 12:33:18 +0700

    Component group input

--- SaleManagement/src/app/management/import/dropdownandaddbutton/dropdownandaddbutton.component.ts
+++ SaleManagement/src/app/management/import/dropdownandaddbutton/dropdownandaddbutton.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/api';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-dropdownandaddbutton',
   templateUrl: './dropdownandaddbutton.component.html',
-  styleUrls: ['./dropdownandaddbutton.component.scss']
+  styleUrls: ['./dropdownandaddbutton.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DropdownandaddbuttonComponent implements OnInit {
+  filteredBrands = [
+    { label: 'Audi', value: 'Audi' },
+    { label: 'BMW', value: 'BMW' },
+    { label: 'Fiat', value: 'Fiat' },
+    { label: 'Ford', value: 'Ford' },
+    { label: 'Honda', value: 'Honda' },
+    { label: 'Jaguar', value: 'Jaguar' },
+    { label: 'Mercedes', value: 'Mercedes' },
+    { label: 'Renault', value: 'Renault' },
+    { label: 'VW', value: 'VW' },
+    { label: 'Volvo', value: 'Volvo' }
+  ];
+  brandName : string;
 
   constructor() { }
 
